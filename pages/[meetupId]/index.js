4618b463bfeda089d441669490d4eb24974b43df
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-09-16 19:54:59 -0300

    fazendo page detalhes reutilizável

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/router';
-import React from 'react';
+import MeetupDetail from "../components/meetups/MeetupDetail"
 
 function MeetupDetailsPage() {
-  const router = useRouter();
-  const meetupId = router.query.meetupId;
-
-  return (
-    <div>
-      <h1>Detalhes do Meetup</h1>
-      <p>ID: {meetupId}</p>
-    </div>
-  );
+    return (
+        <>
+            <MeetupDetail
+                image="https://storage.caosplanejado.com/uploads/2019/05/ruinas.png"
+                title="first meetup"
+                address="some street 5, some city"
+                description="this is a first meetup"
+            />
+        </>
+    );
 }
 
 export default MeetupDetailsPage;
