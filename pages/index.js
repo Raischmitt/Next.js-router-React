fde6ffd599bf7bb7e19fd3af595d5d8e974d2d6d
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-09-15 21:41:36 -0300

    iniciando

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 function HomePage() {
     return (
         <>
             <h1>HomePage</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">NextJS is a great framework</Link></li>
+                <li>Something else</li>
+            </ul>
         </>
     )
 }
---
+++ pages/news/index.js
@@ -0,0 +1,9 @@
+function NewsPage() {
+    return (
+        <>
+            <h1>NewPage</h1>
+        </>
+    )
+}
+
+export default NewsPage;
