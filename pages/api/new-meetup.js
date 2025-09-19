45964e53ca62ccd6bb6ad2f9882eee799a447308
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-09-18 21:09:51 -0300

    enviando api para o mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,21 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
 
-        const { title, image, address, description } = data;
+        const client = await MongoClient.connect('mongodb+srv://raissa:th5SsNRSXui5WM9R@cluster0.luyf3rz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        const db = client.db('meetups');
+
+        const meetupsCollection = db.collection('meetups');
+
+        const result = await meetupsCollection.insertOne({ data });
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({ message: 'Meetup inserted!' });
     }
 }
 
